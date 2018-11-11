72589074ee37c989258d396fcb8a28bc45a937bf
Gizego <32618117+Gizego@users.noreply.github.com>
2018-11-11 18:31:09 +0000

    d

--- build64.bat
+++
@@ -1,3 +0,0 @@
-set path= "C:\Program Files (x86)\mingw-w64\x86_64\mingw64\bin"
-"C:\Program Files (x86)\mingw-w64\x86_64\mingw64\bin\g++.exe" "C:\Users\Gizego\Desktop\Engine86\main.cpp" "C:\Users\Gizego\Desktop\Engine86\include\glad\glad.c" -std=c++17 -O3 -static "./dependencies/x64/libglfw3.a" -static "./dependencies/x64/OpenAL32.lib" -lopengl32 -lgdi32 -o "C:\Users\Gizego\Desktop\Engine86\game64.exe"
-pause
